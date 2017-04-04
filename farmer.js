0b9f34cd19e1e2ad24994ead827c49abf52e2e9b
Marciel Sousa <marcielmj@gmail.com>
2017-04-04 06:07:02 -0300

    logs powered by fancy-log

--- farmer.js
+++ farmer.js
@@ -5,6 +5,7 @@ var Steam = SteamUser.Steam;
 var prompt = require('prompt');
 var request = require('request');
 var Cheerio = require('cheerio');
+var log = require('fancy-log');
 
 var client = new SteamUser({"enablePicsCache": true});
 
@@ -14,18 +15,6 @@ var g_Page = 1;
 var g_CheckTimer;
 var g_OwnedApps = [];
 
-function log(message) {
-	var date = new Date();
-	var time = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
-
-	for(var i = 1; i < 6; i++) {
-		if(time[i] < 10) {
-			time[i] = '0' + time[i];
-		}
-	}
-
-	console.log(time[0] + '-' + time[1] + '-' + time[2] + ' ' + time[3] + ':' + time[4] + ':' + time[5] + ' - ' + message);
-}
 
 var argsStartIdx = 2;
 if(process.argv[0] == 'yasi') {
